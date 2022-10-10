2b56e3dc7ac6cc6d54f256e87a959e283f8c4c4d
wyx218 <114154153+wyx218@users.noreply.github.com>
2022-10-10 08:44:50 +0800

    Add files via upload

---
+++ calculator/pages/result/result.js
@@ -0,0 +1,76 @@
+// pages/result/result.js
+Page({
+
+  data: {
+    result:""
+  },
+
+  onLoad:function(option){
+    var num = wx.getStorageSync('result')
+    this.setData({
+      result: num
+    })
+  },
+
+  ToQuestion: function(options){
+    wx.switchTab({
+      url: '../question/question',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
